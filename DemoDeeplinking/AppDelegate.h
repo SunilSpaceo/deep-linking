a7a837a7e5294e427aa9b2a3b9280af717970da4
SunilSpaceo <sunil@spaceotechnologies.com>
2016-05-20 17:27:39 +0530

    initial commit

---
+++ DemoDeeplinking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoDeeplinking
+//
+//  Created by Sunil on 5/20/16.
+//  Copyright © 2016 Spaceo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoDeeplinking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoDeeplinking
+//
+//  Created by Sunil on 5/20/16.
+//  Copyright © 2016 Spaceo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
